d7777366bddb7e70fa2e93a0fb84b080e15afbe9
Ziya Ul Rehman <ziyaulrehman3@gmail.com>
2025-08-12 13:54:55 +0530

    add voice

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
 
+import express from "express";
 const app = express();
+
+import twilio from "twilio";
 
 const PORT = process.env.PORT || 3000;
 
+const twilioClient = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
+async function callTwilio() {
+  const call = await twilioClient.calls.create({
+    from: process.env.MY_PHONE_NUMBER,
+    to: "+919760999879",
+    url: "http://demo.twilio.com/docs/voice.xml",
+  });
+
+  console.log("Calling to Ziya... ");
+}
+
 app.get("/", (req, res) => {
-  res.send("Hello from Server");
+  res.send("Hello from Twilio App");
+  callTwilio();
+});
+
+app.post("/voice", (req, res) => {
+  const twiml = new twilio.twiml.VoiceResponse();
+  twiml.say("ello, this is a Ziya test call from Younglabs!");
+  res.type("text/xml");
+  res.send(twiml.toString());
 });
 
 app.listen(PORT, (req, res) => {
